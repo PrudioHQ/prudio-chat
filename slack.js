441c3b0ce9c47e10d041bc01c94da26b7cba14a1
Hélder Duarte <helder.duarte@sto.ad>
2014-10-18 18:50:56 +0100

    uptime command, moment.js

--- slack.js
+++ slack.js
@@ -89,8 +89,8 @@ var self = module.exports = {
 				Bots[appid].say(from, "_It's now: *" + moment().utc().format() + "*._");
 			} else if(command === "uptime") {
 				Bots[appid].say(from, "_bootedAt: *" + Bots[appid].bootedAt + "*._");
-				Bots[appid].say(from, "_Time: *" + moment(Bots[appid].bootedAt).format() + "*._");
-				Bots[appid].say(from, "_Uptime: *" + moment(Bots[appid].bootedAt).fromNow() + "*._");
+				Bots[appid].say(from, "_Time: *" + moment().unix(Bots[appid].bootedAt).format() + "*._");
+				Bots[appid].say(from, "_Uptime: *" + moment().unix(Bots[appid].bootedAt).fromNow() + "*._");
 			} else {
 				// Command not valid!
 				Bots[appid].say(from, "_Sorry! Couldn't reconize the command: *" + command + "*._");

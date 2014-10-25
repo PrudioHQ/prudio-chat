e46032b932183c770c8e5d56684bdd18d2441073
Hélder Duarte <helder.duarte@sto.ad>
2014-10-25 18:33:33 +0100

    Event Emitter on data message

--- slack.js
+++ slack.js
@@ -56,13 +56,17 @@ var self = module.exports = {
   	Bots[appid].send('TOPIC', channel, topic);
   },
 
-  connect: function connect(appid, wss) {
+  connect: function connect(appid, connection) {
+
     console.log("Connecting to " + appid);
 
 	if(typeof Bots[appid] === 'undefined') {
 		
-		Bots[appid] = new WebSocket(wss);
+		Bots[appid]             = new WebSocket(connection.url);
 		Bots[appid].isConnected = false;
+		Bots[appid].nick        = connection.self.id;
+		Bots[appid].team        = connection.team.id;
+		Bots[appid].teamDomain  = connection.team.domain;
 		Bots[appid].errors      = [];
 		Bots[appid].bootedAt    = moment().utc().unix();
 
@@ -84,53 +88,96 @@ var self = module.exports = {
 
 		setTimeout(function() {
 			console.log("Try to send message");
-			Bots[appid].send(JSON.stringify({"type":"message","channel":"D02PMQQT2","user":"U02PMQQSY","text":"I am onnnnn","ts": Date.now() + ".000000","team":"T025PLYNL"}));
+			Bots[appid].send(JSON.stringify({"type":"message","channel":"C02R750V5","text":"I am onnnnn with no user!","ts": Date.now() + ".000000"}));
 		}, 8000);
 	});
 
-	Bots[appid].addListener('error', function (e) {
+	Bots[appid].addListener('close', function () {
 		Bots[appid].isConnected = false;
-		Bots[appid].erros.push(e);
-
-		console.log("error");
-		console.log(e);
+		console.log("conn closed");
 	});
 
 	// Direct message
-	Bots[appid].addListener('pm', function (from, message) {
-		console.log("Direct message: " + message);
-		console.log("T: "              + Object.size(Bots));
+	Bots[appid].addListener('direct_message', function (message) {
+		console.log("Direct message: %j", message);
+		console.log("T: " + Object.size(Bots));
 
 		// If command
-		if(message.indexOf("!") == 0 && message.length > 1) {
-			var command = message.substring(1, message.length);
+		if(message.text.indexOf("!") == 0 && message.text.length > 1) {
+			var command = message.text.substring(1, message.text.length);
+			var from = message.user;
 
 			console.log("It's a command: " + command);
 
 			if(command === "time") {
-				Bots[appid].say(from, "_It's now: *" + moment().utc().format() + "*._");
+				self.say(appid, from, "_It's now: *" + moment().utc().format() + "*._");
 			} else if(command === "uptime") {
 				var time = moment(Bots[appid].bootedAt, "X").utc();
-				Bots[appid].say(from, "_Uptime: *" + time.fromNow() + "* @ *" + time.format() + "*._");
+				self.say(appid, from, "_Uptime: *" + time.fromNow() + "* @ *" + time.format() + "*._");
 			} else {
 				// Command not valid!
-				Bots[appid].say(from, "_Sorry! Couldn't reconize the command: *" + command + "*._");
+				self.say(appid, from, "_Sorry! Couldn't reconize the command: *" + command + "*._");
 			}
 
 		} else {
 			// Reply
-			Bots[appid].say(from, "You said: _" + message + "_");
+			//self.say(appid, from, "You said: _" + message.text + "_");
 		}
 
 	});
 
-	Bots[appid].addListener('message', function (message) {
-		console.log("raw dataaaaa " + message);
+	Bots[appid].addListener('message', function (data) {
+		var message = JSON.parse(data);
+
+		if(message.type == 'message' && message.channel.indexOf("C") == 0) {
+
+			console.log("Channel message %j", message);
+			Bots[appid].emit('channel_message', message);
+		
+		} else if(message.type == 'message' && message.channel.indexOf("D") == 0) {
+		
+			console.log("Direct message %j", message);
+			Bots[appid].emit('direct_message', message);
+		
+		} else if(typeof message.type !== 'undefined') {
+		
+			console.log("Other message %s %j", message.type, message);
+			Bots[appid].emit(message.type, message);
+
+		} else {
+		
+			console.log("Undefined message %j", message);
+
+		}
+	});
+
+	Bots[appid].addListener('hello', function () {
+		console.log("Slack said HELLO!!!");
 	});
 
 
 	return Bots[appid];
 
+  },
+
+  say: function say(appid, from, to, message) {
+  	if(typeof Bots[appid] === 'undefined') {
+  		return true;
+	}
+
+	var data = {
+		"type":    "message",
+		"channel": "D02PMQQT2",
+		"user":    "U02PMQQSY",
+		"text":    message,
+		"ts":      Date.now() + ".000000",
+		"team":    "T025PLYNL"
+	};
+
+
+	Bots[appid].send(JSON.stringify(data), function(error) {
+		console.log("Error!");
+	});
   },
   
   disconnect: function disconnect(appid) {
@@ -139,7 +186,7 @@ var self = module.exports = {
   		return true;
 	}
 	
-	Bots[appid].disconnect();
+	Bots[appid].close();
   	Bots[appid] = undefined;
 
   	return true;
@@ -152,13 +199,6 @@ var self = module.exports = {
 	}
 
   	return true;
-  },
-  
-  emitMethod: function emitMethod() {
-  	emitter.emit('emittedevent', x++);
   }
-};
 
-emitter.on('emittedevent', function(x) {
-	console.log('We have got: ' + x);
-});
+};
--- utils/bot.js
+++ utils/bot.js
@@ -9,10 +9,10 @@ module.exports = function(app, slack, models) {
 		//slack.connect(application.id, application.slack_xmpp_user, application.slack_xmpp_pass, application.slack_xmpp_host);
 		request.post(app.get('slack_api_url') + '/rtm.start', { json: true, form: { token: application.slack_api_token, t: Date.now() }}, function (error, response, body) {
 			if (!error && response.statusCode == 200 && typeof body.ok !== "undefined" && body.ok == true) {
-				console.log("Body: %j", body.url);
-				slack.connect(application.id, body.url)
+				//console.log("Body: %j", body.url);
+				slack.connect(application.id, body)
 			} else {
-				console.log("Body: %j", body);
+				//console.log("Body: %j", body);
 			}
 		});
 

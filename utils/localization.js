02d5d62978541334379535702cbbce3b90532768
Helder Duarte <cossou@gmail.com>
2015-05-11 19:21:38 +0100

    Added ES translation

--- utils/localization.js
+++ utils/localization.js
@@ -73,5 +73,30 @@ exports.localization = {
         MINIMIZE: 'Chat minimieren',
         TITLE: 'Support',
         POWERED_BY: '<i class="prd-icon-btn-prudio"></i> Prud.io'
-    }
+    },
+    es: {
+        CHAT_WITH_US: 'Habla con nosotros',
+        NAME: 'Nombre',
+        EMAIL: 'E-mail',
+        START_CONVERSATION: 'Iniciar una conversación',
+        NEW_MESSAGE: 'Nuevo mensaje',
+        NO_USERS_ONLINE: 'Perdon! Actualmente no hay miembros en línea para ayudarle. Deja un mensaje y trataremos de ponernos en contacto lo antes posible!',
+        CONVERSATION_HISTORY: 'Historial de la conversación',
+        JUST_WRITE: 'Escriba aqui...',
+        PROBLEM_CONNECTING_TO_SERVER: 'Hubo un problema al conectar con el servidor!',
+        ERROR_FILL_ALL_FIELDS: 'Por favor, rellene los campos con un nombre y una dirección de correo electrónico válida.',
+        CONNECTING_TO_SERVER: 'Conectando con el servidor...',
+        SERVER_OFFLINE: 'Servidor está apagado!',
+        USER_IS_TYPING: 'El usuario está escribiendo...',
+        UPLOADING_FILE: 'Enviando fichero',
+        ERROR_UPLOADING_FILE: 'Error al adjuntar fichero!',
+        ATTACH_FILE: 'Adjuntar un fichero',
+        USER_OFFLINE: 'La internet se ha ido',
+        USER_ONLINE: 'Estamos de vuelta!',
+        MUTE: 'Silenciar conversación',
+        CLOSE: 'Cerrar conversación',
+        MINIMIZE: 'Minimizar conversación',
+        TITLE: 'Soporte',
+        POWERED_BY: 'Desarrollado por <i class="prd-icon-btn-prudio"></i> Prud.io'
+    },
 };
